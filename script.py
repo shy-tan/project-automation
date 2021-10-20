70dbe23dbc905e1aca44b38bc6d34f2cf2ad8831
Abdullah Bin Tahir <48406637+code-aesthete@users.noreply.github.com>
2021-10-20 16:00:34 +0500

    final script

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from github import Github
+from git import Repo
+
 from helper_classes import MissingRepoNameException
 
 ACCESS_TOKEN = os.environ['GITHUB_ACCESS_TOKEN']
@@ -12,27 +14,31 @@ if len(sys.argv) < 2:
     raise MissingRepoNameException()
 
 # create a github repo with the given name
-
 user = g.get_user()
+project_name = sys.argv[1]
 
 try:
-    repo = user.create_repo(sys.argv[1])
+    repo = user.create_repo(project_name)
 except:
     print('Repo creation failed.')
     quit()
 else:
-    print(f"Repo with name {sys.argv[1]} created!")
+    print(f"Repo with name {project_name} created!")
 
 # make a README.md
+repo.create_file('README.md', "initial commit", f"# {project_name.title()}")
 
-repo.create_file('README.md', "initial commit", f"# {sys.argv[1].title()}")
-
-
-# cd to the right folder
-
+# cd to right folder
 os.chdir(PROJECT_FOLDER)
 
+# create a new folder
+os.system(f'mkdir {project_name}')
+
+# cd to project folder
+os.chdir(project_name)
+
 # clone the repo
-# TODO
+Repo.clone_from(repo.git_url, './')
+
 # open vscode
 os.system('code .')
